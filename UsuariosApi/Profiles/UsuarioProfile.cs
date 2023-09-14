7bc6e2387d05f6dfeb039854bd0180f1f3093a9e
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-09-14 08:20:35 -0300

    Criacao Usuario - Model, Dto, Profile, Controller

---
+++ UsuariosApi/Profiles/UsuarioProfile.cs
@@ -0,0 +1,14 @@
+﻿using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+using AutoMapper;
+
+namespace UsuariosApi.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+        }
+    }
+}
