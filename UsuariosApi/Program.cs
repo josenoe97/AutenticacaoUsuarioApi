776172018d2789462c7f886fdbef46c3e72a8bd2
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-09-15 10:39:54 -0300

    Criacao dos services para descomplexar o controller

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
+using UsuariosApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,9 @@ builder.Services.AddIdentity<Usuario, IdentityRole>()  // Adiciona o conceito de
 
 builder.Services.AddAutoMapper
     (AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<CadastroService>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
